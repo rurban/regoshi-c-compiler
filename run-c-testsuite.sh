37998fdfd737353a7236788725cd34b00b4c9295
Reini Urban <reini.urban@gmail.com>
2026-04-30 18:58:45 +0200

    test: run c-testsuite with -O1

--- run-c-testsuite.sh
+++ run-c-testsuite.sh
@@ -4,5 +4,5 @@ if [ ! -f c-testsuite/single-exec ]; then
     git submodule update --init --recursive
 fi
 cd c-testsuite || exit
-echo "c-testsuite with ../rcc -lm"
-env CC=../rcc CFLAGS=-lm ./single-exec posix | scripts/tapsummary | tee ../c-testsuite.tap.txt
+echo "c-testsuite with ../rcc -O1 -lm"
+env CC="../rcc -O1" CFLAGS=-lm ./single-exec posix | scripts/tapsummary | tee ../c-testsuite.tap.txt
